e481d6aa1857d18e15a982cdb67e15c2b440e921
Samolberg <l.samolberg@khk.ee>
2019-11-25 10:41:11 +0200

    'Katsed' tehtud

--- tunniplaan/src/Katsed.java
+++ tunniplaan/src/Katsed.java
@@ -1,68 +1,81 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Katsed {
     public static void main(String[] args) {
+        // 1. tund
+        // loo Tund objekt
         Tund esimeneTund = new Tund();
-        esimeneTund.aine = "Mobiilirakendus";
-        esimeneTund.algus = "03:30";
-        esimeneTund.grupp = "VS18";
+        // lisa väärtused
+        esimeneTund.tund = "2";
+        esimeneTund.algus = "08:30";
         esimeneTund.lopp = "10:00";
-        esimeneTund.ruum = "407";
+        esimeneTund.aine = "Mobiilirakenduse kasutajaliidese disain";
+        esimeneTund.grupp = "VS18";
         esimeneTund.opetaja = "Ly Otsa";
-        esimeneTund.tund = "1";
+        esimeneTund.ruum = "Kopli A - A418 (arvutiklass)";
 
+        // 2. tund
+        // loo Tund objekt
         Tund teineTund = new Tund();
-        teineTund.aine = "Mobiilirakendus";
+        // lisa väärtused
+        teineTund.tund = "3";
         teineTund.algus = "10:15";
-        teineTund.grupp = "VS18";
         teineTund.lopp = "11:45";
-        teineTund.ruum = "411";
+        teineTund.aine = "Mobiilirakenduse kasutajaliidese disain";
+        teineTund.grupp = "VS18";
         teineTund.opetaja = "Ly Otsa";
-        teineTund.tund = "2";
+        teineTund.ruum = "Kopli A - A418 (arvutiklass)";
 
+        // 3. tund
+        // loo Tund objekt
         Tund kolmasTund = new Tund();
-        kolmasTund.aine = "JAVA";
+        // lisa väärtused
+        kolmasTund.tund = "4";
         kolmasTund.algus = "11:55";
-        kolmasTund.grupp = "VS18";
         kolmasTund.lopp = "14:00";
-        kolmasTund.ruum = "411";
+        kolmasTund.aine = "JAVA programmeerimine";
+        kolmasTund.grupp = "VS18";
         kolmasTund.opetaja = "Anna Karutina";
-        kolmasTund.tund = "3";
+        kolmasTund.ruum = "Kopli A - A411 (arvutiklass)";
 
+        // 4. tund
+        // loo Tund objekt
         Tund neljasTund = new Tund();
-        neljasTund.aine = "JAVA";
-        neljasTund.algus = "13:55";
+        // lisa väärtused
+        neljasTund.tund = "5";
+        neljasTund.algus = "14:10";
+        neljasTund.lopp = "15:40";
+        neljasTund.aine = "JAVA programmeerimine";
         neljasTund.grupp = "VS18";
-        neljasTund.lopp = "15:00";
-        neljasTund.ruum = "411";
         neljasTund.opetaja = "Anna Karutina";
-        neljasTund.tund = "4";
+        neljasTund.ruum = "Kopli A - A411 (arvutiklass)";
 
+        // 5. tund
+        // loo Tund objekt
         Tund viiesTund = new Tund();
-        viiesTund.aine = "JAVA";
-        viiesTund.algus = "15:00";
+        // lisa väärtused
+        viiesTund.tund = "6";
+        viiesTund.algus = "15:45";
+        viiesTund.lopp = "17:15";
+        viiesTund.aine = "JAVA programmeerimine";
         viiesTund.grupp = "VS18";
-        viiesTund.lopp = "17:00";
-        viiesTund.ruum = "411";
         viiesTund.opetaja = "Anna Karutina";
-        viiesTund.tund = "5";
+        viiesTund.ruum = "Kopli A - A411 (arvutiklass)";
 
-        //loo päev
+        // loo päev
         Paev esmaspaev = new Paev();
         esmaspaev.kuupaev = "2019-11-18";
-        esmaspaev.tundideNimekiri = new ArrayList<Tund>;
-        //lisame tunnid päevale
-        esmaspaev.tundideNimekiri.add(esimeneTund);
-        esmaspaev.tundideNimekiri.add(teineTund);
-        esmaspaev.tundideNimekiri.add(kolmasTund);
-        esmaspaev.tundideNimekiri.add(neljasTund);
-        esmaspaev.tundideNimekiri.add(viiesTund);
+        // lisame tunnid päevale
+        ArrayList<Tund> tunnid = new ArrayList<Tund>();
+        tunnid.add(esimeneTund);
+        tunnid.add(teineTund);
+        tunnid.add(kolmasTund);
+        tunnid.add(neljasTund);
+        tunnid.add(viiesTund);
+        esmaspaev.tundideNimekiri = tunnid;
 
-        System.out.println(esimeneTund);
-        System.out.println(teineTund);
-        System.out.println(kolmasTund);
-        System.out.println(neljasTund);
-        System.out.println(viiesTund);
 
+        System.out.println(esmaspaev);
     }
 }
