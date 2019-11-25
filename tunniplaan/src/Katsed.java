4b5d11a8d1dd05b5757eeb2b1b7f446dd5ec6b9b
Samolberg <l.samolberg@khk.ee>
2019-11-25 10:39:17 +0200

    Päeva kirjeldus

--- tunniplaan/src/Katsed.java
+++ tunniplaan/src/Katsed.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+
 public class Katsed {
     public static void main(String[] args) {
-        Tund kolmasTund = new Tund();
+        Tund esimeneTund = new Tund();
+        esimeneTund.aine = "Mobiilirakendus";
+        esimeneTund.algus = "03:30";
+        esimeneTund.grupp = "VS18";
+        esimeneTund.lopp = "10:00";
+        esimeneTund.ruum = "407";
+        esimeneTund.opetaja = "Ly Otsa";
+        esimeneTund.tund = "1";
 
+        Tund teineTund = new Tund();
+        teineTund.aine = "Mobiilirakendus";
+        teineTund.algus = "10:15";
+        teineTund.grupp = "VS18";
+        teineTund.lopp = "11:45";
+        teineTund.ruum = "411";
+        teineTund.opetaja = "Ly Otsa";
+        teineTund.tund = "2";
+
+        Tund kolmasTund = new Tund();
         kolmasTund.aine = "JAVA";
         kolmasTund.algus = "11:55";
         kolmasTund.grupp = "VS18";
@@ -11,7 +30,6 @@ public class Katsed {
         kolmasTund.tund = "3";
 
         Tund neljasTund = new Tund();
-
         neljasTund.aine = "JAVA";
         neljasTund.algus = "13:55";
         neljasTund.grupp = "VS18";
@@ -20,8 +38,31 @@ public class Katsed {
         neljasTund.opetaja = "Anna Karutina";
         neljasTund.tund = "4";
 
+        Tund viiesTund = new Tund();
+        viiesTund.aine = "JAVA";
+        viiesTund.algus = "15:00";
+        viiesTund.grupp = "VS18";
+        viiesTund.lopp = "17:00";
+        viiesTund.ruum = "411";
+        viiesTund.opetaja = "Anna Karutina";
+        viiesTund.tund = "5";
+
+        //loo päev
+        Paev esmaspaev = new Paev();
+        esmaspaev.kuupaev = "2019-11-18";
+        esmaspaev.tundideNimekiri = new ArrayList<Tund>;
+        //lisame tunnid päevale
+        esmaspaev.tundideNimekiri.add(esimeneTund);
+        esmaspaev.tundideNimekiri.add(teineTund);
+        esmaspaev.tundideNimekiri.add(kolmasTund);
+        esmaspaev.tundideNimekiri.add(neljasTund);
+        esmaspaev.tundideNimekiri.add(viiesTund);
+
+        System.out.println(esimeneTund);
+        System.out.println(teineTund);
         System.out.println(kolmasTund);
         System.out.println(neljasTund);
+        System.out.println(viiesTund);
 
     }
 }
---
+++ tunniplaan/src/Paev.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+
+public class Paev {
+    public String kuupaev;
+    public List<Tund> tundideNimekiri;
+
+    @Override
+    public String toString() {
+        return "Paev: " +
+                "\nkuupaev: " + kuupaev +
+                "\nTunnid: " + tundideNimekiri +
+                "\n"
+                ;
+    }
+}
